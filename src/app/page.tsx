dadc0c7e826a64e10b9d4f4478559339c78ca0ea
ayodyabanuka <ayodyabanukafernando@gmail.com>
2024-08-30 13:31:19 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,27 +40,27 @@ export default function Home() {
 
 
   return (
-    <div className="container mx-auto flex flex-col justify-center min-h-screen items-center p-10 gap-16 text-[#E9E9E9] numbers">
+    <div className="container mx-auto flex flex-col justify-center min-h-screen items-center p-10 gap-16 text-[#E9E9E9] numbers ">
       <Image src={"/logo1.png"} alt={""} height={2000} width={2000} className="h-44 w-fit" />
-      <div className="flex gap-10 justify-center items-center px-10 py-7 border border-[#E9E9E9] rounded-3xl  font-tnum font-tabular-nums">
+      <div className="flex gap-5 md:gap-10 justify-center items-center px-10 py-7 border border-[#E9E9E9] rounded-3xl  font-tnum font-tabular-nums">
         <div className="flex flex-col justify-center items-center">
-          <div className="font-medium text-sm md:text-lg lg:text-lg">DAYS</div>
+          <div className="font-medium text-xs md:text-lg lg:text-lg">DAYS</div>
           <div className="font-extrabold text-4xl md:text-6xl lg:text-8xl  font-tnum font-tabular-nums">{timeLeft.days}</div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <div className="font-medium text-sm md:text-lg lg:text-lg">HOURS</div>
+          <div className="font-medium text-xs md:text-lg lg:text-lg">HOURS</div>
           <div className="font-extrabold text-4xl md:text-6xl lg:text-8xl  font-tnum font-tabular-nums">{timeLeft.hours}</div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <div className="font-medium text-sm md:text-lg lg:text-lg">MINUTES</div>
+          <div className="font-medium text-xs md:text-lg lg:text-lg">MINUTES</div>
           <div className="font-extrabold text-4xl md:text-6xl lg:text-8xl  font-tnum font-tabular-nums">{timeLeft.minutes}</div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <div className="font-medium text-sm md:text-lg lg:text-lg">SECONDS</div>
+          <div className="font-medium text-xs md:text-lg lg:text-lg">SECONDS</div>
           <div className="font-extrabold text-4xl md:text-6xl lg:text-8xl font-tnum font-tabular-nums">{timeLeft.seconds}</div>
         </div>
       </div>
-      <div className="text-5xl md:text-6xl lg:text-9xl font-extrabold">
+      <div className="text-4xl md:text-6xl lg:text-9xl font-extrabold">
         COMING SOON
       </div>
       <div className="flex flex-col gap-5 items-center justify-center">
